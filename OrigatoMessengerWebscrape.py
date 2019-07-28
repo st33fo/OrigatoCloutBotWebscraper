b17944a116ad28752887e720a788ac5cf8e1e05f
Stef <swp5ab@virginia.edu>
2019-07-28 00:11:19 -0400

    Now reading messages in chat. Does not separate speaker from message. React numbers also appear in the text. Timestamps not included either.

--- OrigatoMessengerWebscrape.py
+++ OrigatoMessengerWebscrape.py
@@ -8,7 +8,9 @@ Created on Tue Jul 23 23:10:03 2019
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from flask import Flask,request, render_template
-import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 app = Flask(__name__)
 #I should comment my code
 
@@ -24,23 +26,25 @@ def login():
     username = driver.find_element_by_id("email")
     password = driver.find_element_by_id("pass")
     
-#    userinput = input("Username Please")
-#    userpass = input("Password Please")
+   # userinput = input("Username Please")
+   # userpass = input("Password Please")
     
-    userinput = 'your user name'
-    userpass = 'and password'
+    userinput = 'Your user name'
+    userpass = 'Your password'
     username.send_keys(userinput)
     password.send_keys(userpass)
-    messages =[]
+    messageslist =[]
     driver.find_element_by_name("login").click()
     
     page = driver.find_element_by_class_name("_2sdm")
     subpage = page.find_element_by_class_name("_4_j4")
-    subpage2 = subpage.find_elements_by_xpath('//div')
-   
-    allelements = len(subpage2)
-    for subs in range(allelements):
-        messages.append(subpage2[subs])
+   #subpage2 = subpage.find_elements_by_xpath('//div')
+    wait = WebDriverWait(subpage,10)
+    subpage2 = subpage.find_element_by_css_selector('._4u-c._1wfr._9hq')
+
+    
+    messages = subpage2.find_elements_by_css_selector('._1t_p.clearfix')
+    
     
     return render_template('Messages.html', messages = messages)
 
